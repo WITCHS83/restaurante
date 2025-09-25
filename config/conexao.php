df274c2fd5c69cd53b62ed6be24c5566ed36cbb9
anderson.witchs <anderson.witchs@ufrn.br>
2025-09-25 16:50:47 -0300

    ajuste conexao logar.php e login.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,15 +1,29 @@
-<?php 
-error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-// ALTERE OS DADOS DAS STRINGS (NOMES QUE ESTÃO ENTRE AS ASPAS)
-$host = "sql302.infinityfree.com"; // endereco do banco de dados
-$usuario = "if0_40003100"; // usuario do banco de dados
-$senhadobanco = "AdCmW141719"; // senha do banco de dados
-$nomedobanco = "if0_40003100_restnovo"; //nome do banco de dados
+<?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
+// Dados de acesso
+$host         = "sql302.infinityfree.com";
+$usuario      = "if0_40003100";
+$senhadobanco = "AdCmW141719";
+$nomedobanco  = "if0_40003100_restnovo";
+
+// Inicializa conexão
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $usuario, $senhadobanco, $nomedobanco, 3306, null, MYSQLI_CLIENT_MULTI_RESULTS);
-if (!$conn) {
+mysqli_real_connect(
+    $conn,
+    $host,
+    $usuario,
+    $senhadobanco,
+    $nomedobanco,
+    3306,
+    null,
+    MYSQLI_CLIENT_MULTI_RESULTS
+);
+
+// Verifica erro de conexão
+if (mysqli_connect_errno()) {
     die("Falha na conexão MySQL: " . mysqli_connect_error());
 }
+
+// Define charset
 mysqli_set_charset($conn, "utf8mb4");
-?>
